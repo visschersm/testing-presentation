11d901854c4591dfc077e631b530a1373f85efe9
Mark Visschers <visschers.m@gmail.com>
2021-10-13 23:36:48 +0200

    testing presentation

---
+++ method-hiding/Program.cs
@@ -0,0 +1,25 @@
+﻿Base foo = new Base();
+foo.Method();
+
+Derived bah = new Derived();
+bah.Method();
+
+Base bar = new Derived();
+bar.Method();
+
+
+public class Base
+{
+    public virtual void Method()
+    {
+        Console.WriteLine("Hello, World");
+    }
+}
+
+public class Derived : Base
+{
+    public override void Method()
+    {
+        Console.WriteLine("Goodbye, World");
+    }
+}
